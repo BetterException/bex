434a1d8d1359586d220a05e97bd3f8af31b08b48
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2024-09-20 18:37:37 +0530

    add: added a lot of files to gitignore and fixed the testing file

--- test/testFile.cpp
+++ test/testFile.cpp
@@ -4,12 +4,9 @@ int main(){
     try {
         cout << "Hello World";
     } catch(exception& e) {
-std::cout << "[BEX] Exception: 0";
         cout << "Some boring exception";
     } catch (...) {
-std::cout << "[BEX] Exception: 1";
         cout << "Inside exception";
     }
     return 0;
 }
-
