aaade5d33509bdbb1bac80b2d005715311ebc700
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2024-09-21 15:02:00 +0530

    add: added checks for OpenSSL

--- bex/main.cpp
+++ bex/main.cpp
@@ -5,7 +5,21 @@
 
 int main(int argc, char *argv[])
 {
+    std::cout << "888888b.\n";
+    std::cout << "888  \"88b\n";
+    std::cout << "888  .88P\n";
+    std::cout << "8888888K.   .d88b.  888  888\n";
+    std::cout << "888  \"Y88b d8P  Y8b `Y8bd8P'\n";
+    std::cout << "888    888 88888888   X88K\n";
+    std::cout << "888   d88P Y8b.     .d8\"\"8b.\n";
+    std::cout << "8888888P\"   \"Y8888  888  888\n";
+
     int filesModified = 0;
+#if __has_include("openssl/sha.h")
+#else
+    std::cout << "OpenSSL not found! Exiting." << std::endl;
+    exit(1);
+#endif
     std::string path = ".";
     if (argc == 2)
     {
@@ -31,15 +45,6 @@ int main(int argc, char *argv[])
             ++filesModified;
         }
     }
-
-    std::cout << "888888b.\n";
-    std::cout << "888  \"88b\n";
-    std::cout << "888  .88P\n";
-    std::cout << "8888888K.   .d88b.  888  888\n";
-    std::cout << "888  \"Y88b d8P  Y8b `Y8bd8P'\n";
-    std::cout << "888    888 88888888   X88K\n";
-    std::cout << "888   d88P Y8b.     .d8\"\"8b.\n";
-    std::cout << "8888888P\"   \"Y8888  888  888\n";
 
     if(filesModified == 0) {
         std::cout << "No files modified" << std::endl;
