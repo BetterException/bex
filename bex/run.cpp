5b7ecb9572fbae9f5206bb205fddac34ed57e332
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2024-09-24 19:13:58 +0530

    add: added support for modifying multiple files at once

--- bex/run.cpp
+++ bex/run.cpp
@@ -11,25 +11,29 @@ void run(int argc, char *argv[], bool createDummyFiles) {
   std::vector<std::future<bool>> futures;
   int filesModified = 0;
   std::string path = ".";
-  if (argc == 2) {
-    path = argv[1];
-  }
-  std::filesystem::path pPath(path);
-  if (!pPath.is_relative()) {
-    pPath = pPath.relative_path();
-  }
-  if (std::filesystem::is_directory(pPath)) {
-    for (const auto &entry :
-         std::filesystem::recursive_directory_iterator(pPath)) {
-      if (entry.is_directory()) continue;
-      if (!entry.is_symlink() || !entry.is_socket()) {
-        std::filesystem::path tempPath(entry);
+  if (argc >= 2) {
+    int index = 1;
+    while (index < argc) {
+      path = argv[index++];
+      std::filesystem::path pPath(path);
+      if (!pPath.is_relative()) {
+        pPath = pPath.relative_path();
+      }
+      if (std::filesystem::is_directory(pPath)) {
+        for (const auto &entry :
+             std::filesystem::recursive_directory_iterator(pPath)) {
+          if (entry.is_directory()) continue;
+          if (!entry.is_symlink() || !entry.is_socket()) {
+            std::filesystem::path tempPath(entry);
+            futures.push_back(
+                std::async(process, tempPath.string(), createDummyFiles));
+          }
+        }
+      } else {
         futures.push_back(
-            std::async(process, tempPath.string(), createDummyFiles));
+            std::async(process, pPath.string(), createDummyFiles));
       }
     }
-  } else {
-    futures.push_back(std::async(process, pPath.string(), createDummyFiles));
   }
 
   for (auto &future : futures) {
