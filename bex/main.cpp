132bcbdbd0692889d522a78d647bfe89b9f0fe7b
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2024-12-17 01:23:51 +0530

    add: added a missing }

--- bex/main.cpp
+++ bex/main.cpp
@@ -19,8 +19,7 @@ int main(int argc, char* argv[]) {
     std::cout << "888  \"Y88b d8P  Y8b `Y8bd8P'\n";
     std::cout << "888    888 88888888   X88K\n";
     std::cout << "888   d88P Y8b.     .d8\"\"8b.\n";
-  std::cout << "8888888P\"   \"Y8888  888  888\n";
-
+  }
 #if __has_include("openssl/sha.h")
 #else
   std::cout << "OpenSSL not found! Exiting." << std::endl;
