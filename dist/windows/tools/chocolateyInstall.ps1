82cbcda28f6ac9a7e2da18d60f8c4ea8a189d4a5
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2025-01-02 19:29:37 +0000

    Release: 0.2

--- dist/windows/tools/chocolateyInstall.ps1
+++ dist/windows/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageName= 'bex'
 $version = "0.2"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = "https://github.com/BetterException/bex/releases/download/$version/bex-win64.zip"
-$hash = "0f32cd9b0aff25a1c63310b16158e3213875391d6cfdc8e4d4e0f668820a82f5"
+$hash = "64358e3ea77709893e887034bd0b1c3a3069996182b2a40d00f7fc4562667396"
 
 $packageArgs = @{
   packageName   = $packageName
