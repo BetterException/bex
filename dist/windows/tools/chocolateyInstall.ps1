a470eb5407e3b014baa435929301794267903aba
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2025-01-02 21:36:44 +0530

    Revert "Release: 0.2"
    This reverts commit 31b0fdbd83ba62305b4e2e778450fc8e96603a10.

--- dist/windows/tools/chocolateyInstall.ps1
+++ dist/windows/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName= 'bex'
-$version = "0.2"
+$version = '0.1'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = "https://github.com/BetterException/bex/releases/download/$version/bex-win64.zip"
-$hash = "cbdff213fe58f27066a6ab6a5fe8e38766aceac0b8c7be76ccb332e8d1c691c1"
+$hash = 'ac80624dbb8712dd61117a3f28ddb7a1ede9ca17f98d30297a253ba760e17284'
 
 $packageArgs = @{
   packageName   = $packageName
